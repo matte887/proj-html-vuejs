138dbe68c3eadebcf243b682b938e2e9e298ab67
matte887 <m.erroi92@gmail.com>
2022-05-30 17:36:40 +0200

    Featured seconda sezione: implementata

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -4,7 +4,11 @@
       <div class="ms_container">
         <div class="container-fluid">
           <h5 class="small-title mb-3">Avada forum is the place to be</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur nam cupiditate, ullam veritatis esse nemo recusandae dolor ipsa optio reprehenderit labore.</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur
+            nam cupiditate, ullam veritatis esse nemo recusandae dolor ipsa
+            optio reprehenderit labore.
+          </p>
         </div>
       </div>
     </section>
@@ -15,22 +19,45 @@
       <div class="container ms_news-wrapper">
         <div class="row row-cols-3 gx-5">
           <div class="col">
-            <img src="../assets/images/selected/post_feat_img_25-700x441.jpg" alt="">
+            <img
+              src="../assets/images/selected/post_feat_img_25-700x441.jpg"
+              alt=""
+            />
             <h5>Morbi vitae dui euismod vulputate siollicitudin</h5>
-            <p class="ms_small-text">October 11th, 2015 | <span>2 Comments</span></p>
-            <p class="ms_test-grey">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo voluptatem quia explicabo laboriosam inventore praesentium, a maxime.</p>
+            <p class="ms_small-text">
+              October 11th, 2015 | <span>2 Comments</span>
+            </p>
+            <p class="ms_test-grey">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+              voluptatem quia explicabo laboriosam inventore praesentium, a
+              maxime.
+            </p>
           </div>
           <div class="col">
-            <img src="../assets/images/selected/post_feat_img_24-700x441.jpg" alt="">
+            <img
+              src="../assets/images/selected/post_feat_img_24-700x441.jpg"
+              alt=""
+            />
             <h5>Vivamus pellenteque, felis quis varius</h5>
             <p class="ms_small-text">October 11th, 2015 | Comments off</p>
-            <p class="ms_test-grey">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo voluptatem quia explicabo laboriosam inventore praesentium, a maxime.</p>
+            <p class="ms_test-grey">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+              voluptatem quia explicabo laboriosam inventore praesentium, a
+              maxime.
+            </p>
           </div>
           <div class="col">
-            <img src="../assets/images/selected/post_feat_img_23-700x441.jpg" alt="">
+            <img
+              src="../assets/images/selected/post_feat_img_23-700x441.jpg"
+              alt=""
+            />
             <h5>Donec ornare pretium erget scelisque justo</h5>
             <p class="ms_small-text">October 11th, 2015 | Comments off</p>
-            <p class="ms_test-grey">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo voluptatem quia explicabo laboriosam inventore praesentium, a maxime.</p>
+            <p class="ms_test-grey">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+              voluptatem quia explicabo laboriosam inventore praesentium, a
+              maxime.
+            </p>
           </div>
         </div>
       </div>
@@ -38,9 +65,15 @@
 
     <section class="ms_featured">
       <div class="container">
-        <h5 class="ms_small-title mb-3" id="featured-white">Featured article</h5>
+        <h5 class="ms_small-title mb-3" id="featured-white">
+          Featured article
+        </h5>
         <h3 class="ms_h3-title">Cras malesuada et orci eget pharetra</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae eligendi sint atque sapiente praesentium fuga recusandae cum dolore consequuntur esse.</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+          eligendi sint atque sapiente praesentium fuga recusandae cum dolore
+          consequuntur esse.
+        </p>
         <a href="" class="ms_btn ms_dark-btn">
           Read more
           <i class="fa-solid fa-angle-right"></i>
@@ -52,22 +85,108 @@
       <div class="container ms_news-wrapper">
         <div class="row row-cols-3 gx-5">
           <div class="col">
-            <img src="../assets/images/selected/post_feat_img_22-700x441.jpg" alt="">
+            <img
+              src="../assets/images/selected/post_feat_img_22-700x441.jpg"
+              alt=""
+            />
             <h5>Morbi vitae dui euismod vulputate siollicitudin</h5>
-            <p class="ms_small-text">October 11th, 2015 | <span>2 Comments</span></p>
-            <p class="ms_test-grey">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo voluptatem quia explicabo laboriosam inventore praesentium, a maxime.</p>
+            <p class="ms_small-text">
+              October 11th, 2015 | <span>2 Comments</span>
+            </p>
+            <p class="ms_test-grey">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+              voluptatem quia explicabo laboriosam inventore praesentium, a
+              maxime.
+            </p>
           </div>
           <div class="col">
-            <img src="../assets/images/selected/post_feat_img_21-700x441.jpg" alt="">
+            <img
+              src="../assets/images/selected/post_feat_img_21-700x441.jpg"
+              alt=""
+            />
             <h5>Vivamus pellenteque, felis quis varius</h5>
             <p class="ms_small-text">October 11th, 2015 | Comments off</p>
-            <p class="ms_test-grey">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo voluptatem quia explicabo laboriosam inventore praesentium, a maxime.</p>
+            <p class="ms_test-grey">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+              voluptatem quia explicabo laboriosam inventore praesentium, a
+              maxime.
+            </p>
           </div>
           <div class="col">
-            <img src="../assets/images/selected/post_feat_img_20-700x441.jpg" alt="">
+            <img
+              src="../assets/images/selected/post_feat_img_20-700x441.jpg"
+              alt=""
+            />
             <h5>Donec ornare pretium erget scelisque justo</h5>
             <p class="ms_small-text">October 11th, 2015 | Comments off</p>
-            <p class="ms_test-grey">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo voluptatem quia explicabo laboriosam inventore praesentium, a maxime.</p>
+            <p class="ms_test-grey">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+              voluptatem quia explicabo laboriosam inventore praesentium, a
+              maxime.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="ms_reviews-tutorials">
+      <div class="container">
+        <div class="row">
+          <div class="col-8">
+            <div class="ms_reviewed">
+              <h5 class="ms_small-title mb-3" id="featured-white">
+                Reviewed product
+              </h5>
+              <h3 class="ms_h3-title">
+                Mauris viverra arisan ipsum eget felis prims efficitur
+              </h3>
+              <p>
+                Amet saepe labore enim nemo, soluta quam tenetur alias debitis
+                sapiente doloremque in? Officia a perferendis dolorum
+                recusandae, doloremque soluta itaque quae.
+              </p>
+              <a href="" class="ms_btn ms_dark-btn">
+                Read more
+                <i class="fa-solid fa-angle-right"></i>
+              </a>
+            </div>
+          </div>
+          <div class="col-4 d-flex flex-column">
+            <h4 class="feat-title">Tutorials & Guides</h4>
+            <div class="ms_tutorial-guides-wrapper d-flex flex-column justify-content-around flex-grow-1">
+              <div class="d-flex">
+                <img
+                  src="../assets/images/selected/post_feat_img_23-177x142.jpg"
+                  alt=""
+                />
+                <div class="ms_description">
+                  <h5>Donec ornare pretium erget scelisque justo</h5>
+                  <p class="ms_small-text">October 11th, 2015 | Comments off</p>
+                </div>
+              </div>
+              <div class="d-flex">
+                <img
+                  src="../assets/images/selected/post_feat_img_22-177x142.jpg"
+                  alt=""
+                />
+                <div class="ms_description">
+                  <h5>Morbi vitae dui euismod vulputate siollicitudin</h5>
+                  <p class="ms_small-text">
+                    October 11th, 2015 | <span>2 Comments</span>
+                  </p>
+                </div>
+              </div>
+              <div class="d-flex">
+                <img
+                  src="../assets/images/selected/post_feat_img_20-177x142.jpg"
+                  alt=""
+                />
+                <div class="ms_description">
+                  <h5>Donec ornare pretium erget scelisque justo</h5>
+                  <p class="ms_small-text">October 11th, 2015 | Comments off</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -77,13 +196,13 @@
 
 <script>
 export default {
-    name: 'AppMain',
-}
+  name: "AppMain",
+};
 </script>
 
 <style lang="scss" scoped>
-@import '../style/variables.scss';
-@import '../style/common.scss';
+@import "../style/variables.scss";
+@import "../style/common.scss";
 
 .ms_disclaimer {
   text-align: center;
@@ -113,8 +232,9 @@ export default {
   }
 }
 
+// News common style
 .ms_news,
-.ms_news-second{
+.ms_news-second {
   .ms_news-wrapper {
     text-align: initial;
     h5 {
@@ -128,30 +248,59 @@ export default {
     }
   }
 }
-
+// /News common style
 
 .ms_featured {
   color: $white-shade;
   margin-top: 4rem;
   .container {
     background-image: url(../assets/images/selected/featured_article_1_bg.jpg);
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-position: center;
+
     padding: 17rem 7rem 7rem 7rem;
-    #featured-white {
-      color:$white-shade;
-    }
-    p {
-      margin-bottom: 3rem;
-    }
-    .ms_btn {
-      padding: 1rem 3rem;
-    }
   }
 }
 
+// Featured common style
+.ms_featured .container,
+.ms_reviewed {
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  #featured-white {
+    color: $white-shade;
+  }
+  p {
+    margin-bottom: 3rem;
+  }
+  .ms_btn {
+    padding: 1rem 3rem;
+  }
+}
+// /Featured common style
+
 .ms_news-second {
   margin-top: 5rem;
+}
+
+.ms_reviews-tutorials {
+  .ms_reviewed {
+    color: $white-shade;
+    background-image: url(../assets/images/selected/featured_article_2_bg.jpg);
+    padding: 6rem;
+  }
+  .feat-title::after {
+    width: 5rem;
+    height: 2px;
+    content: "";
+    display: block;
+    margin: 1.5rem 0;
+    background-color: $accent-color;
+  }
+
+  .ms_tutorial-guides-wrapper img {
+    width: 30%;
+    height: auto;
+    margin-right: 1rem;
+  }
 }
 </style>
