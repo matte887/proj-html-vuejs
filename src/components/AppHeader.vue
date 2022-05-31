aed7df0401d14cb5ac8c086294b2f4856fb34292
matte887 <m.erroi92@gmail.com>
2022-05-31 12:22:11 +0200

    Implementato triangolo header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -31,14 +31,14 @@
       </div>
     </header>
     <div class="ms_jumbotron d-flex align-items-center">
-        <div class="container">
-            <h1>Tech Forum!</h1>
-            <h3 class="mb-5">Discuss all the latest technology new and trends</h3>
-            <a class="ms_btn ms_primary-btn" href="">
-                Get Avada Now!
-                <i class="fa-solid fa-angle-right"></i>
-            </a>
-        </div>
+      <div class="container">
+        <h1>Tech Forum!</h1>
+        <h3 class="mb-5">Discuss all the latest technology news and trends</h3>
+        <a class="ms_btn ms_primary-btn" href="">
+          Get Avada Now!
+          <i class="fa-solid fa-angle-right"></i>
+        </a>
+      </div>
     </div>
   </div>
 </template>
@@ -56,39 +56,52 @@ export default {
 @import "../style/variables.scss";
 @import "~@fortawesome/fontawesome-free/css/all.min.css";
 .ms_header-container {
-    color: $white-shade;
-    header {
+  color: $white-shade;
+  header {
     width: 100%;
     height: $header-height;
     background-color: $brand-primary-color;
     color: $white-shade;
     .ms_logo {
-        height: 60%;
-        width: auto;
+      height: 60%;
+      width: auto;
     }
     nav {
-        width: 70%;
-        ul {
+      width: 70%;
+      ul {
         margin-bottom: 0;
-        }
+      }
     }
-    }
+  }
 
-    .ms_jumbotron {
-        width: 100%;
-        height: 75vh;
-        background-image: url(../assets/images/selected/home_slider_bg.jpg);
-        background-repeat: no-repeat;
-        background-position: center;
-        background-size: cover;
+  .ms_jumbotron {
+    width: 100%;
+    height: 75vh;
+    background-image: url(../assets/images/selected/home_slider_bg.jpg);
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
 
-        h1 {
-            font-size: 8rem;
-            font-weight: 700;
-        }
-        h3 {
-            font-weight: 400;
-        }
+    h1 {
+      font-size: 8rem;
+      font-weight: 700;
     }
+    h3 {
+      font-weight: 400;
+    }
+  }
+}
+
+header::after {
+  content: "";
+  display: inline-block;
+  width: 0;
+  height: 0;
+  border-left: 12px solid transparent;
+  border-right: 12px solid transparent;
+  border-top: 13px solid $brand-primary-color;
+  position: relative;
+  left: 765px;
+  top: -5px;
 }
 </style>
